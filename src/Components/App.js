ccbaf8579857388f36f9dee9d3c846744a3efed9
Nate Jensen <najens@gmail.com>
2018-04-19 00:22:56 -0600

    docs: Add updateBookStatus(book) to App component

--- src/Components/App.js
+++ src/Components/App.js
@@ -32,6 +32,48 @@ class App extends Component {
         this.getBooks()
     }
 
+    /**
+     * Send API request to update book shelf and update state
+     *
+     * @param {object} book containing at minimum an `id` attribute
+     * @param {string} shelf contains one of ["wantToRead", "currentlyReading", "read", "none"]
+     */
+    updateShelf = (book, shelf) => {
+        BooksAPI.update(book, shelf)
+            .then(() => {
+                book.shelf = shelf
+                this.updateBookStatus(book)
+            })
+    }
+
+    /**
+     * Update the state for all books and the bookshelves
+     *
+     * @param {object} book
+     */
+    updateBookStatus = (book) => {
+        const books = this.state.books;
+        const bookIndex = books
+            .findIndex((item) => item.id === book.id);
+
+        if (bookIndex !== -1) {
+            if (book.shelf === 'none') {
+                books.splice(bookIndex, 1);
+            } else {
+                books[bookIndex] = book;
+            }
+        } else {
+            books.push(book);
+        }
+
+        this.setState({
+            books: books,
+            reading: books.filter((book) => book.shelf === 'currentlyReading'),
+            toRead: books.filter((book) => book.shelf === 'wantToRead'),
+            read: books.filter((book) => book.shelf === 'read')
+        });
+    };
+
     render() {
         return (
             <div className="App">
