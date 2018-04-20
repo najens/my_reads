19505761ed45fa1598ef69abc929164356f60017
Nate Jensen <najens@gmail.com>
2018-04-20 00:02:10 -0600

    docs: Convert BookShelf to functional component

--- src/Components/BookShelf.js
+++ src/Components/BookShelf.js
@@ -5,37 +5,33 @@ import Book from './Book';
 /**
  * Stateless component that displays a bookshelf
  */
-class BookShelf extends React.Component {
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        bookList: PropTypes.array.isRequired,
-        onUpdateShelf: PropTypes.func.isRequired
-    }
+ const BookShelf = ({ title, bookList, onUpdateShelf }) => {
+     return (
+         <div className="bookshelf">
+             <h2 className="bookshelf-title">{ title }</h2>
+             <div className="bookshelf-books">
+                 <ol className="books-grid">
+                     {/*
+                         Map through each book in the shelf
+                         and render the book component
+                     */}
+                     {bookList.map((book) => (
+                         <Book
+                             key={book.id}
+                             book={book}
+                             onUpdateShelf={onUpdateShelf}
+                         />
+                     ))}
+                 </ol>
+             </div>
+         </div>
+     );
+ };
 
-    render () {
-        const { title, bookList, onUpdateShelf } = this.props;
-
-        return (
-            <div className="bookshelf">
-                <h2 className="bookshelf-title">{ title }</h2>
-                <div className="bookshelf-books">
-                    <ol className="books-grid">
-                        {/*
-                            Map through each book in the shelf
-                            and render the book component
-                        */}
-                        {bookList.map((book) => (
-                            <Book
-                                key={book.id}
-                                book={book}
-                                onUpdateShelf={onUpdateShelf}
-                            />
-                        ))}
-                    </ol>
-                </div>
-            </div>
-        );
-    }
-}
+ BookShelf.propTypes = {
+     title: PropTypes.string.isRequired,
+     bookList: PropTypes.array.isRequired,
+     onUpdateShelf: PropTypes.func.isRequired
+ }
 
 export default BookShelf;
